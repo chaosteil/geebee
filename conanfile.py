4e1ff8e52b13a673cd8d4e6adc4b4c6b75bd4937
Dominykas Djacenko <chaosteil@gmail.com>
2016-03-24 11:34:43 +0200

    Reset conanfile.

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,3 @@ class GeeBee(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "Boost/1.60.0@lasote/stable", "sdl/2.0.4@chaosteil/stable"
     generators = "cmake"
-
-    def config(self):
-        self.settings.compiler.libcxx = 'libstdc++11'
