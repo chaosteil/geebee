8ef809676b334241defa15d96d853d549ebceb1b
Dominykas Djacenko <chaosteil@gmail.com>
2016-03-24 11:08:30 +0200

    Upgraded conanfile to use libstdc++11 by default

--- conanfile.py
+++ conanfile.py
@@ -4,3 +4,6 @@ class GeeBee(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "Boost/1.60.0@lasote/stable", "sdl/2.0.4@chaosteil/stable"
     generators = "cmake"
+
+    def config(self):
+        self.settings.compiler.libcxx = 'libstdc++11'
